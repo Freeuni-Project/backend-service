2e845adf0ed74e7d5d88ed52d8ba26304d7efcf4
David <dtarg16@freeuni.edu.ge>
2021-08-29 16:43:27 +0400

    solve heartbeat problem

--- producer.py
+++ producer.py
@@ -1,13 +1,12 @@
 import json
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
-channel = connection.channel()
-channel.queue_declare(queue='stat_queue', durable=True)
-
 
 def publish(method, body):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    channel = connection.channel()
+    channel.queue_declare(queue='stat_queue', durable=True)
     properties = pika.BasicProperties(method, delivery_mode=2)
 
     channel.basic_publish(exchange='', routing_key='stat_queue', body=json.dumps(body), properties=properties)
-
+    connection.close()
